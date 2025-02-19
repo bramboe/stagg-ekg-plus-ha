8ad44741195076207e61c0c82a699ca986323377
bramboe <37293908+bramboe@users.noreply.github.com>
2025-02-19 22:34:02 +0100

    Update const.py

--- custom_components/fellow_stagg/const.py
+++ custom_components/fellow_stagg/const.py
@@ -4,6 +4,8 @@ DOMAIN = "fellow_stagg"
 SERVICE_UUID = "B4DF5A1C-3F6B-F4BF-EA4A-82030490-1A02"
 CHAR_UUID = "B4DF5A1C-3F6B-F4BF-EA4A-820350FF1A02"
 
-# The magic init sequence (in hex) used to authenticate with the kettle:
-# f1 ce 12 08 2d c8 24 c1 f3 00 97 c1 3a 48 0e 67 5c 89 4a ba
-INIT_SEQUENCE = bytes.fromhex("f1ce12082dc824c1f30097c13a480e675c894aba")
+# The magic init sequence for the EKG-2d-25-b0 kettle:
+# First write "ESP" to proto-ver characteristic (handle 0x0015)
+# Then write a sequence to prov-session (handle 0x000F)
+# Then write to prov-scan characteristic (handle 0x000C)
+INIT_SEQUENCE = bytes.fromhex("455350100125A2012220889794D1273C492FD635D0DD20AD3F972C0CE3B95D4FB4B5B24D2EAD51DD4EABE3ED637744")
