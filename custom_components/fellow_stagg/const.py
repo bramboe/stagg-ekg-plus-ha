4275cc326608161fae460226ba6f16169d2a523d
bramboe <37293908+bramboe@users.noreply.github.com>
2025-02-19 22:24:57 +0100

    Update const.py

--- custom_components/fellow_stagg/const.py
+++ custom_components/fellow_stagg/const.py
@@ -5,5 +5,5 @@ SERVICE_UUID = "B4DF5A1C-3F6B-F4BF-EA4A-82030490-1A02"
 CHAR_UUID = "B4DF5A1C-3F6B-F4BF-EA4A-820350FF1A02"
 
 # The magic init sequence (in hex) used to authenticate with the kettle:
-# 10 01 5a 25 a2 01 22 0a 20 a4 1c 79 9e b1 23 8c b2 3a 45 93
-INIT_SEQUENCE = bytes.fromhex("10015a25a20122a0a204a1c799eb1238cb23a4593")
+# 45 53 50 10 01 5a 25 a2 01 22 0a 20 a4 1c 79 9e b1 23 8c b2
+INIT_SEQUENCE = bytes.fromhex("45535010015a25a20122a0a204a1c799eb1238cb2")
