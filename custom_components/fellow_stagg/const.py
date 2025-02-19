3d7f93ebe53025b6a7cc460faaf8040b96ac0c8f
bramboe <37293908+bramboe@users.noreply.github.com>
2025-02-19 21:47:04 +0100

    Update const.py

--- custom_components/fellow_stagg/const.py
+++ custom_components/fellow_stagg/const.py
@@ -1,8 +1,8 @@
 DOMAIN = "fellow_stagg"
 
 # BLE UUIDs for the Fellow Stagg kettle’s “Serial Port Service”
-SERVICE_UUID = "00001820-0000-1000-8000-00805f9b34fb"
-CHAR_UUID = "00002A80-0000-1000-8000-00805f9b34fb"
+SERVICE_UUID = "B4DF5A1C-3F6B-F4BF-EA4A-82030490-1A02"
+CHAR_UUID = "B4DF5A1C-3F6B-F4BF-EA4A-820350FF1A02"
 
 # The magic init sequence (in hex) used to authenticate with the kettle:
 # ef dd 0b 30 31 32 33 34 35 36 37 38 39 30 31 32 33 34 9a 6d
