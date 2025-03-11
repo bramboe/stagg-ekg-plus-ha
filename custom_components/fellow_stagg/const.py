7d1a0de5355b03b987bd78f004b1a737189814d1
bramboe <37293908+bramboe@users.noreply.github.com>
2025-03-11 22:46:44 +0100

    Update const.py

--- custom_components/fellow_stagg/const.py
+++ custom_components/fellow_stagg/const.py
@@ -1,9 +1,9 @@
 DOMAIN = "fellow_stagg"
 
-# BLE UUIDs for the Fellow Stagg kettle’s “Serial Port Service”
-SERVICE_UUID = "00001820-0000-1000-8000-00805f9b34fb"
-CHAR_UUID = "00002A80-0000-1000-8000-00805f9b34fb"
+# BLE UUIDs for the Fellow Stagg EKG Pro kettle based on logs
+SERVICE_UUID = "021A9004-0382-4AEA-BFF4-6B3F1C5ADFB4"
+CHAR_UUID = "021AFF50-0382-4AEA-BFF4-6B3F1C5ADFB4"
 
-# The magic init sequence (in hex) used to authenticate with the kettle:
-# ef dd 0b 30 31 32 33 34 35 36 37 38 39 30 31 32 33 34 9a 6d
-INIT_SEQUENCE = bytes.fromhex("efdd0b3031323334353637383930313233349a6d")
+# The EKG Pro appears to use a simpler initialization approach
+# Just using the header for now - we'll rely on standard BLE procedure
+INIT_SEQUENCE = bytes.fromhex("efdd0b")
