99f842e2c2338298ded43ae851e576bfbadf783c
bramboe <37293908+bramboe@users.noreply.github.com>
2025-03-11 17:43:00 +0100

    Update const.py

--- custom_components/fellow_stagg/const.py
+++ custom_components/fellow_stagg/const.py
@@ -7,10 +7,10 @@ SERVICE_UUID = "021a9004-0382-4aea-bff4-6b3f1c5adfb4"
 CONTROL_SERVICE_UUID = "7aebf330-6cb1-46e4-b23b-7cc2262c605e"
 
 # Control service characteristic for notifications
-CHAR_CONTROL_UUID = "2291c4b5-5d7f-4477-a88b-b266edb97142"
+CHAR_CONTROL_UUID = "2291c4b1-5d7f-4477-a88b-b266edb97142"
 
 # Control service characteristic for writing commands
-CHAR_WRITE_UUID = "2291c4b7-5d7f-4477-a88b-b266edb97142"
+CHAR_WRITE_UUID = "2291c4b2-5d7f-4477-a88b-b266edb97142"
 
 # We may not need a magic init sequence based on the logs
 INIT_SEQUENCE = None
