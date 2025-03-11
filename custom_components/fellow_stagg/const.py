42d17db4f7962f3a360b864850403cfd2274dbe8
bramboe <37293908+bramboe@users.noreply.github.com>
2025-03-11 18:04:52 +0100

    Update const.py

--- custom_components/fellow_stagg/const.py
+++ custom_components/fellow_stagg/const.py
@@ -12,5 +12,24 @@ CHAR_CONTROL_UUID = "2291c4b1-5d7f-4477-a88b-b266edb97142"
 # Control service characteristic for writing commands
 CHAR_WRITE_UUID = "2291c4b2-5d7f-4477-a88b-b266edb97142"
 
-# We may not need a magic init sequence based on the logs
-INIT_SEQUENCE = None
+# Initialization sequence based on observed BLE patterns
+INIT_SEQUENCE = [
+    # First service characteristics
+    {
+        "uuid": "021aff50-0382-4aea-bff4-6b3f1c5adfb4",
+        "data": bytearray([0xF7, 0x0A, 0x00, 0x00, 0x01, 0x00, 0x00, 0x00, 0x00])
+    },
+    {
+        "uuid": "021aff51-0382-4aea-bff4-6b3f1c5adfb4",
+        "data": bytearray([0xF7, 0x0B, 0x00, 0x00, 0x01, 0x00, 0x00, 0x00, 0x00])
+    },
+    # Control service characteristics
+    {
+        "uuid": "2291c4b1-5d7f-4477-a88b-b266edb97142",
+        "data": bytearray([0xF7, 0x0C, 0x00, 0x00, 0x01, 0x00, 0x00, 0x00, 0x00])
+    },
+    {
+        "uuid": "2291c4b2-5d7f-4477-a88b-b266edb97142",
+        "data": bytearray([0xF7, 0x0D, 0x00, 0x00, 0x01, 0x00, 0x00, 0x00, 0x00])
+    }
+]
