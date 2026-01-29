a75bc4cbe5e172a9901d4f279a8c8e12b8cb5e76
bramboe <37293908+bramboe@users.noreply.github.com>
2026-01-29 18:01:12 +0100

    Update const.py

--- custom_components/fellow_stagg/const.py
+++ custom_components/fellow_stagg/const.py
@@ -1,5 +1,10 @@
 DOMAIN = "fellow_stagg"
 
+# Config entry options
+OPT_TEMPERATURE_UNIT = "temperature_unit"
+OPT_TEMPERATURE_UNIT_C = "C"
+OPT_TEMPERATURE_UNIT_F = "F"
+
 # Default temperature limits (C / F) for the EKG Pro Wi‑Fi CLI API
 MIN_TEMP_F = 104
 MAX_TEMP_F = 212
