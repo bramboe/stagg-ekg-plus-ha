03d35925b4c9e5a57b2daf18bc5b6d34497b172c
bramboe <37293908+bramboe@users.noreply.github.com>
2026-01-31 22:53:02 +0100

    Update __init__.py

--- custom_components/fellow_stagg/__init__.py
+++ custom_components/fellow_stagg/__init__.py
@@ -232,7 +232,7 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     hass.data[DOMAIN]["services_registered"] = True
 
   hass.data.setdefault(DOMAIN, {})[entry.entry_id] = coordinator
-  entry.async_add_update_listener(_async_options_updated)
+  entry.add_update_listener(_async_options_updated)
 
   await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
 
