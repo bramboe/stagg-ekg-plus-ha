e4e67b65184bd697731ceb670d9c10486156bc57
bramboe <37293908+bramboe@users.noreply.github.com>
2026-01-31 23:44:55 +0100

    Update const.py

--- custom_components/fellow_stagg/const.py
+++ custom_components/fellow_stagg/const.py
@@ -17,3 +17,10 @@ OPT_POLLING_INTERVAL_COUNTDOWN = "polling_interval_countdown_seconds"
 
 # Default path for the kettle HTTP CLI endpoint
 CLI_PATH = "/cli"
+
+# BLE (EKG+ protocol, from tlyakhov/fellow-stagg-ekg-plus)
+# Serial Port Service / characteristic used for kettle commands and state
+SERVICE_UUID = "00001820-0000-1000-8000-00805f9b34fb"
+CHAR_UUID = "00002a80-0000-1000-8000-00805f9b34fb"
+# Magic init sequence to talk to the kettle (hex: efdd 0b 3031 3233 3435 3637 3839 3031 3233 34 9a 6d)
+INIT_SEQUENCE = bytes([0xEF, 0xDD, 0x0B, 0x30, 0x31, 0x32, 0x33, 0x34, 0x35, 0x36, 0x37, 0x38, 0x39, 0x30, 0x31, 0x32, 0x33, 0x34, 0x9A, 0x6D])
